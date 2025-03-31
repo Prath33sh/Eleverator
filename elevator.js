22ce49329697558979c08adda898abcabb06826e
pratheesh kumar <pratheeshkumarm@gmail.com>
2025-03-31 07:30:34 -0400

    initial working copy

---
+++ elevator.js
@@ -0,0 +1,205 @@
+import { parentPort } from 'worker_threads';
+
+const State = {
+    Error: -1,
+    Stopped: 0,
+    Running: 1 
+};
+
+const Direction = {
+    Down: 0,
+    Up: 1
+};
+
+const MAX_OCCUPANCY = 10;
+
+export class Elevator {
+    constructor(floor, occupancy, state) {
+        this.floor = floor;
+        this.occupancy = occupancy;
+        this.state = state;
+        this.upwardList = [0];
+        this.downwardList = [0];
+        this.direction = Direction.Up; // Initial state
+    }
+
+    onDestination(floor, direction) {
+        this.setFloor(floor);
+        console.log(`Elevator open at Floor: ${this.floor}`);
+        parentPort.postMessage({event: 'doorOpen', data: this});
+        this.updateList(direction);
+    }
+
+    getFloor() {
+        return this.floor;
+    }
+
+    setFloor(floor) {
+        this.floor = floor;
+    }
+
+    setState(state) {
+        this.state = state;
+    }
+
+    resetToBaseLevel() {
+        this.upwardList = [0];
+        this.downwardList = [0];
+    }
+
+    canUse() {
+        return this.state !== State.Error && this.occupancy < MAX_OCCUPANCY;
+    }
+
+
+
+    openRequest(userFloor) {
+        let direction;
+        let diff = this.floor - userFloor;
+        if (diff === 0) {
+            console.log(`Elevator open at Floor: ${this.floor}.`);
+            parentPort.postMessage({event: 'doorOpen', data: this});
+            return;
+        } else if (diff > 0) {
+            direction = Direction.Up;
+        } else {
+            direction = Direction.Down;
+        }
+
+        if (!this.canUse()) {
+            console.log(`Elevator full or error. Please wait. You are at: ${userFloor} , elevator at: ${this.floor}`);
+            this.addToWaitList(userFloor, direction);
+            return;
+        }
+
+        if (userFloor !== this.floor) {
+            console.log(`Elevator in use. Please wait. You are at: ${userFloor} , elevator at: ${this.floor}`);
+            this.addToWaitList(userFloor, direction);
+            return;
+        } 
+    }
+
+    async userSelect(destinations) {     
+        for (let i = 0; i < destinations.length; i++) {
+            const userFloor = destinations[i];
+            let direction;
+            let diff = this.floor - userFloor;
+            if (diff === 0) {
+            console.log(`Elevator open at Floor: ${this.floor}. User can exit now.`);
+            await new Promise(resolve => setTimeout(resolve, 1000)); 
+            } else if (diff > 0) {
+            direction = Direction.Down;
+            } else {
+            direction = Direction.Up;
+            }
+        
+            if (!this.canUse()) {
+            console.log(`Elevator full or error. Please wait. You are at: ${userFloor}, elevator at: ${this.floor}`);
+            this.addToWaitList(userFloor, direction);
+            continue;
+            }
+        
+            if (userFloor !== this.floor) {
+            console.log(`Elevator in use. Please wait. You are at: ${userFloor}, elevator at: ${this.floor}`);
+            this.addToWaitList(userFloor, direction);
+            continue;
+            }
+        }
+    }
+
+    addToWaitList(floor, direction) {
+        if (direction === Direction.Up) {
+            if (this.upwardList.find((f) => f === floor) === undefined) {
+                this.upwardList.push(floor);
+                this.upwardList.sort((a, b) => a - b); // Sort the list to maintain order
+            }
+        } else if (direction === Direction.Down) {
+            if (this.downwardList.find((f) => f === floor) === undefined) {
+                this.downwardList.push(floor);
+                this.downwardList.sort((a, b) => b - a); // Sort the list in descending order for downward direction
+            }
+        }
+    }
+
+    updateList(direction) {
+        if (direction === Direction.Up) {
+            this.upwardList.shift();
+        } else if (direction === Direction.Down) {
+            this.downwardList.shift();
+        }
+    }
+
+    getNextFloor() {
+        let nextFloor = null;
+        if (this.direction === Direction.Up){
+            if(this.upwardList.length > 0) {
+                nextFloor = this.upwardList[0];
+            }
+            else if (this.downwardList.length > 0) {
+                nextFloor = this.downwardList[0];
+                this.direction = Direction.Down; // Change direction if there are no more upward floors
+            }
+        } else if (this.direction === Direction.Down){           
+            if(this.downwardList.length > 0) {
+                nextFloor = this.downwardList[0];
+            }
+            else if (this.upwardList.length > 0) {
+                nextFloor = this.upwardList[0];
+                this.direction = Direction.Up; // Change direction if there are no more upward floors
+            }
+        }
+        return nextFloor;
+    }
+
+    async runElevator(){
+        while (this.state === State.Running) {          
+            if (this.upwardList.length > 0 || this.downwardList.length > 0) {
+              let nextFloor = this.getNextFloor();
+              await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1000ms 
+              this.onDestination(nextFloor, this.direction);
+            } else {
+              // If no messages, wait a bit before looping again
+              // this prevents the loop from consuming too much CPU
+              this.setState(State.Stopped); // Stop the elevator if there are no more floors to go to
+              await new Promise(resolve => setTimeout(resolve, 100)); // Wait 100ms
+              console.log('No more floors to go. Stopping elevator.');
+            }
+            parentPort.postMessage({event: 'status', data: this});
+        }       
+    }
+}
+
+const elevator = new Elevator(0, 0, State.Stopped);
+
+parentPort.on('message', async (event) => {
+    const { data } = event;
+    switch (data.type) {
+        case 'userSelect': {
+            console.log(`User input received at Floor: ${data.userFloor} to Destinations: ${data.inputs}`);
+            await elevator.userSelect(data.inputs);
+            if (elevator.state === State.Stopped) {
+                elevator.setState(State.Running); // Change state to Running if we have a valid request
+                await elevator.runElevator(); // Run elevator if stopped
+             } 
+            break;
+        }
+        case 'openRequest': {
+            console.log(`User requested elevator at Floor: ${data.userFloor}`);
+            elevator.openRequest(data.userFloor);
+            if (elevator.state === State.Stopped) {
+                elevator.setState(State.Running); // Change state to Running if we have a valid request
+                await elevator.runElevator(); // Run elevator if stopped
+             } 
+            break;
+        }
+        case 'onStop': {
+            elevator.setState(State.Stopped);
+            break;
+        }
+        
+        default: {
+            console.log('Received error message from controller.');
+            elevator.setState(State.Error);
+        }
+    }
+});
---
+++ main.js
@@ -0,0 +1,64 @@
+import { Worker } from 'worker_threads';
+
+const userInputs = new Map();
+userInputs.set(0, [4, 5, 3, 7, 9]);
+userInputs.set(2, [1, 5, 5, 7, 9]);
+userInputs.set(5, [3, 7, 9]);
+userInputs.set(3, [6, 5, 3, 4, 9]);
+userInputs.set(8, [9, 5, 3, 7, 9]);
+userInputs.set(4, [4, 2, 8, 6]);
+userInputs.set(1, [3]);
+userInputs.set(7, [1, 5, 3, 7, 9]);
+
+function getElevatoreState(state){
+    switch (state) {
+        case -1:
+            return 'Error';
+        case 0:
+            return 'Stopped';
+        case 1:
+            return 'Running';
+        default:
+            return 'Unknown State';
+    }
+}
+
+async function run(){
+    console.log('Starting elevator simulation...');
+    const worker = new Worker('./elevator.js');
+    Array.from(userInputs.keys()).forEach(async input => {
+        await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 3000) + 1000));
+        console.log(`===> User inputs for floor ${input}: ${userInputs.get(input).join(', ')}`);
+        worker.postMessage({ data: {type: 'openRequest', userFloor: input}});
+    });
+    
+    worker.on('message', async (event) => {
+        const { event: eventType, data: elevator } = event;
+        if (eventType === 'doorOpen') {
+            console.log(`[[< --- >]] Door opened at floor ${elevator.floor}. Please exit/enter the elevator and set destinations.`);
+            // Simulate door open time
+            await new Promise(resolve => setTimeout(resolve, 200)); // 2/10 seconds for door open
+            console.log(`Door closed at floor ${elevator.floor}`);
+            if (elevator.floor !== null && userInputs.has(elevator.floor)){
+                worker.postMessage({ 
+                    data: {type: 'userSelect', 
+                    userFloor: elevator.floor,
+                    inputs: userInputs.get(elevator.floor)}});
+                userInputs.delete(elevator.floor); // prevent reprocessing the inputs upon door open
+            }
+        } else if (eventType === 'status') {
+            console.log(`[[]] Current Elevator Status: ${getElevatoreState(elevator.state)}, Floor: ${elevator.floor}, Direction: ${elevator.direction === 0 ? 'down' : 'up'}, 
+                Upward List: ${elevator.upwardList}, Downward List: ${elevator.downwardList}`);
+        }
+    });
+    worker.on('error', (error) => {
+        console.error('Worker error:', error);
+    });
+    worker.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(`Worker stopped with exit code ${code}`);
+        }
+    });
+}
+
+await run();
